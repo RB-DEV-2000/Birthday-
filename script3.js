7b1c8724590e1b11291d2f8b5042e7e91dc820b2
RB DEV <83820423+RB-DEV-2000@users.noreply.github.com>
2024-04-20 18:13:17 +0530

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,3 +1,13 @@
-setTimeout(function() {
-    window.location.href = 'message.html'; // Redirect to a thank you page
-}, 3000);
+function eatChocolate() {
+    var chocolate = document.getElementById('chocolate');
+    var chocolateHeading = document.getElementById('chocolateHeading');
+
+    // Hide the chocolate and chocolate heading when clicked
+    chocolate.style.display = 'none';
+    chocolateHeading.style.display = 'none';
+
+    // Redirect to the message page after 3 seconds (3000 milliseconds)
+    setTimeout(function() {
+        window.location.href = 'message.html';
+    }, 3000);
+}
