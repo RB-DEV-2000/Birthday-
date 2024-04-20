7492744716befc4f62f276c6bc8958284acb4407
RB DEV <83820423+RB-DEV-2000@users.noreply.github.com>
2024-04-20 18:07:30 +0530

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,13 +1,3 @@
-function eatChocolate() {
-    var chocolate = document.getElementById('chocolate');
-    var chocolateHeading = document.getElementById('chocolateHeading');
-
-    // Hide the chocolate and chocolate heading when clicked
-    chocolate.style.display = 'none';
-    chocolateHeading.style.display = 'none';
-
-    // Close the browser after 3 seconds (3000 milliseconds)
-    setTimeout(function() {
-        window.close();
-    }, 3000);
-}
+setTimeout(function() {
+    window.location.href = 'message.html'; // Redirect to a thank you page
+}, 3000);
