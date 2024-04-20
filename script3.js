78aab444a70a9b67ccb5ec2ae5d27ae90d5903e6
RB DEV <83820423+RB-DEV-2000@users.noreply.github.com>
2024-04-20 18:15:21 +0530

    Update script3.js

--- script3.js
+++ script3.js
@@ -6,8 +6,6 @@ function eatChocolate() {
     chocolate.style.display = 'none';
     chocolateHeading.style.display = 'none';
 
-    // Redirect to the message page after 3 seconds (3000 milliseconds)
-    setTimeout(function() {
-        window.location.href = 'message.html';
-    }, 3000);
+    // Redirect to the message page
+    window.location.href = 'message.html';
 }
